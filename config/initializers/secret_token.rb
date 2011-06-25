c5d1a7c59a0acf6f89396f18e5122b6a5aa45d02
TiSer <toss35@ua.fm>
2011-06-25 20:47:39 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shmitter::Application.config.secret_token = '8506f22cf75e0da4e6f0c19bdf8b889595ea73f1d9c3cd1c4fbe1c849c4ee304aced5b0f70e1741e419ba1b8924bf683d2f1ec387ba46415d2d6ee3d84b41fea'
