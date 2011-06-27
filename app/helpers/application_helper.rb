a916e7cf2e214c44774ae4f958ccab06d5015f30
TiSer <toss35@ua.fm>
2011-06-28 02:08:39 +0300

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def title
+    base_title = "Shmitter"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+  def logo
+    logo = image_tag("logo.jpg", :alt => "Sample App", :class => "round")
+  end
+
 end
