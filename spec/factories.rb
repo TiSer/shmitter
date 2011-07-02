b057da64a5e3f949ce5fc4edb2ed4306c5c90f7e
TiSer <toss35@ua.fm>
2011-07-03 02:20:11 +0300

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Sergey Racoon"
+  user.email                 "toss35@ua.fm"
+  user.password              "pwdpwd"
+  user.password_confirmation "pwdpwd"
+end
