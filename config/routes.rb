e8061a67f2e2ecd3d3a256669b16ecfd4d8eb102
TiSer <toss35@ua.fm>
2011-07-02 10:18:32 +0300

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Shmitter::Application.routes.draw do
 #  get "pages/about"
 #  get "pages/help"
 
+  resources :users
+
   match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-
-
 
   root :to => 'pages#home'
 
